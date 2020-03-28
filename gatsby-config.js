2bc80b0b8b51145f9d93f0ce42c1d7d6ef282cb2
Deven Bansod <devenbansod.bits@gmail.com>
2020-03-28 11:28:58 -0400

    Try adding Google Analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -21,5 +21,21 @@ module.exports = {
     },
     'gatsby-plugin-sass',
     'gatsby-plugin-offline',
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // The property ID; the tracking code won't be generated without it
+        trackingId: "UA-133375103-1",
+        // Defines where to place the tracking script - `true` in the head and `false` in the body
+        head: false,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true,
+        sampleRate: 5,
+        siteSpeedSampleRate: 10,
+        cookieDomain: "devenbansod.dev",
+      },
+    },
   ],
 };
